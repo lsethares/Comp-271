f8f82bf672b8fa1616ad487ad98a2cf059c7fe3c
Leandra Sethares <lsethares@luc.edu>
2016-04-26 19:19:42 -0500

    pril 26

--- HW6_hash/main.cpp
+++ HW6_hash/main.cpp
@@ -10,8 +10,8 @@ using namespace std;
 bool getFile (char* filepath, int *& my_array)      //gets file from filepath
 {
     if(my_array!=NULL) delete my_array;
-    my_array = new int[500];                   //sets up array for integers to go into
-    char lineData[50];
+    my_array = new int[45000000];                   //sets up array for integers to go into
+    char lineData[10];
     int number;
     ifstream in;
     in.open(filepath);
@@ -23,7 +23,7 @@ bool getFile (char* filepath, int *& my_array)      //gets file from filepath
     int i=0;
     while(in.peek()!=EOF)
     {
-        in.getline(lineData,50,' ');
+        in.getline(lineData,10,',');
         number=atoi(lineData);
         my_array[i]=number;
         i++;
@@ -62,7 +62,7 @@ int hash (int val)
 
     temp = digits.top() * 10;
     digits.pop();
-    index = index + temp + digits.top();
+    index = index + temp + digits.top() + 1;
     return index;
 }
 
@@ -94,11 +94,43 @@ bool quad_probing_without_replacement(int key, int hash_table[], int M)
 
 int main()
 {
-    int SSarray[899999999];
+    int *array_of_numbers = new int[45000000];
 
-    cout << hash(123456789) << endl;
-    cout << hash(987654321) << endl;
-    cout << hash(111111111) << endl;
-    cout << hash(000000000) << endl;
-    return 0;
+    getFile("everybodys_socials.txt",array_of_numbers);     //read in SSNs to array
+
+    int M;          //get M to use in quad probing
+    cout << "Enter a number from 0 to 45,000,000: ";
+    cin >> M;
+
+    int nSS = 45000000;        //nSS is the number of social security numbers;
+    int *hash_table=new int [nSS];
+    int index;
+
+    for(int i=0;i<M;i++)    //initialize the hash table to contain 0s
+        hash_table[i]=0;
+
+    for(int i=0;i<M;i++)    //hash the elements in array_of_numbers into the hash_table
+    {
+        index=hash(array_of_numbers[i]);
+        if(hash_table[index]==0)        //position is empty
+            hash_table[index]=array_of_numbers[i];
+        else                            //position is already filled -> collision!
+        {
+            cout << "COLLISION! Fear not... I'll resolve it!\n";
+            quad_probing_without_replacement(array_of_numbers[i],hash_table,M);
+        }
+    }
+
+    ofstream out;                               //open new o fstream to write results to
+    out.open("hashed_socials.txt");
+
+    for (int i=0; i<(nSS-1); i++)               //write out all but the last value of the hash_table
+    {
+        out << hash_table[i] << " , ";
+    }
+
+    out << hash_table[nSS-1] << endl;     //write out last value of the hash_table
+    out.close();                                //close out fstream
+
+    return 1;
 }
